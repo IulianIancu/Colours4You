b6d63f51902853872347fc945538c1b8a887a04e
Julian <julian.iancu@pulselive.com>
2020-04-17 14:27:17 +0100

    Step 1, simple layout that changes between 2 hardcoded colours.

--- app/src/main/java/com/colors/you/ui/main/MainFragment.kt
+++ app/src/main/java/com/colors/you/ui/main/MainFragment.kt
@@ -1,5 +1,6 @@
 package com.colors.you.ui.main
 
+import android.graphics.Color
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,6 +8,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.colors.you.R
+import com.colors.you.observe
+import kotlinx.android.synthetic.main.main_fragment.*
 
 class MainFragment : Fragment() {
 
@@ -24,7 +27,32 @@ class MainFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        refresh.setOnClickListener {
+            viewModel.getRandom()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        viewModel.apply {
+            observe(isLoading,::handleLoadingsState)
+            observe(colorCode,::handleNewColor)
+        }
+        viewModel.getRandom()
+    }
+
+    private fun handleLoadingsState(state:Boolean?){
+        //TODO Do some loading animation?
+    }
+
+    private fun handleNewColor(color:String?){
+        val hex = Color.parseColor(color)
+        background.setBackgroundColor(hex)
+    }
+
 
 }
--- app/src/main/java/com/colors/you/ui/main/MainViewModel.kt
+++ app/src/main/java/com/colors/you/ui/main/MainViewModel.kt
@@ -1,7 +1,20 @@
 package com.colors.you.ui.main
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlin.random.Random
 
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    val error: MutableLiveData<String> = MutableLiveData()
+    val isLoading: MutableLiveData<Boolean> = MutableLiveData()
+    val colorCode: MutableLiveData<String> = MutableLiveData()
+
+    fun getRandom() {
+        colorCode.value = when (Random.nextBoolean()) {
+            true -> "#ff2244"
+            false -> "#a81567"
+        }
+
+    }
 }
