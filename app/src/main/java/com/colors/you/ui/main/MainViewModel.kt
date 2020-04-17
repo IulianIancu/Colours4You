523e854ffc112abcfa017bdf68ac8cad59fafcb6
Julian <julian.iancu@pulselive.com>
2020-04-17 14:52:29 +0100

    Ooops, actually use the repository

--- app/src/main/java/com/colors/you/ui/main/MainViewModel.kt
+++ app/src/main/java/com/colors/you/ui/main/MainViewModel.kt
@@ -3,19 +3,15 @@ package com.colors.you.ui.main
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.colors.you.domain.RandomColorRepository
-import kotlin.random.Random
 
-class MainViewModel(colorRepository: RandomColorRepository) : ViewModel() {
+class MainViewModel(private val colorRepository: RandomColorRepository) : ViewModel() {
 
     val error: MutableLiveData<String> = MutableLiveData()
     val isLoading: MutableLiveData<Boolean> = MutableLiveData()
     val colorCode: MutableLiveData<String> = MutableLiveData()
 
     fun getRandom() {
-        colorCode.value = when (Random.nextBoolean()) {
-            true -> "#ff2244"
-            false -> "#a81567"
-        }
+        colorCode.value = colorRepository.getColor()
 
     }
 }
