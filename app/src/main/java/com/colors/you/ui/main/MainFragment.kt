0dc88d9861c0c54124a08804875737642fa8d6c3
Julian <julian.iancu@pulselive.com>
2020-04-17 15:51:37 +0100

    Bonus set text color to be the reverse of the genereated color.

--- app/src/main/java/com/colors/you/ui/main/MainFragment.kt
+++ app/src/main/java/com/colors/you/ui/main/MainFragment.kt
@@ -11,6 +11,7 @@ import com.colors.you.R
 import com.colors.you.observe
 import kotlinx.android.synthetic.main.main_fragment.*
 
+
 class MainFragment : Fragment() {
 
     companion object {
@@ -53,9 +54,28 @@ class MainFragment : Fragment() {
     }
 
     private fun handleNewColor(color: String?) {
-        val hex = Color.parseColor(color)
-        background.setBackgroundColor(hex)
+        //If there is no color just wait
+        if (color == null) return
+
+        val regularHex = Color.parseColor(color)
+        val invertedHex = getInvertedColor(regularHex)
+
+        background.setBackgroundColor(regularHex)
+
+        //This is jut me being extra and making it so the text should be readable
+        message.setTextColor(invertedHex)
     }
 
+    private fun getInvertedColor(hexa: Int): Int { //hexa = "#28cb43";
+        val hsv = FloatArray(3)
+        Color.RGBToHSV(
+            Color.red(hexa),
+            Color.green(hexa),
+            Color.blue(hexa),
+            hsv
+        )
+        hsv[0] = (hsv[0] + 180) % 360
+        return Color.HSVToColor(hsv)
+    }
 
 }
